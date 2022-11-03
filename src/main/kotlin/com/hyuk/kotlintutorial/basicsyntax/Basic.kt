af3a2435bb6dad78fa6dfbb450f03c73c1b933ee
hyuk0309 <bahngeh@naver.com>
2022-11-04 00:18:00 +0900

    feat : practice basic syntax

---
+++ src/main/kotlin/com/hyuk/kotlintutorial/basicsyntax/Basic.kt
@@ -0,0 +1,11 @@
+package com.hyuk.kotlintutorial.basicsyntax
+
+class Basic {
+    fun sum(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    fun sub(a: Int, b:Int): Int {
+        return a - b
+    }
+}
---
+++ src/test/kotlin/com/hyuk/kotlintutorial/basicsyntax/BasicTest.kt
@@ -0,0 +1,61 @@
+package com.hyuk.kotlintutorial.basicsyntax
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
+
+internal class BasicTest {
+
+    private var basic = Basic();
+
+    @Test
+    fun read_only_변수_선언() {
+        //given
+        val a = 10
+        val b = 20
+
+        //when then
+        assertAll(
+            { assertThat(a).isEqualTo(10) },
+            { assertThat(b).isEqualTo(20) }
+        )
+    }
+
+    @Test
+    fun 일반_변수_선언() {
+        //given
+        var v = 10
+
+        //when
+        v = 20
+
+        //then
+        assertThat(v).isEqualTo(20)
+    }
+
+    @Test
+    fun sum_함수() {
+        //given
+        var a = 10
+        var b = 20
+
+        //when
+        var result = basic.sum(a, b)
+
+        //then
+        assertThat(result).isEqualTo(a + b);
+    }
+
+    @Test
+    fun sub_함수() {
+        //given
+        var a = 20
+        var b = 10
+
+        //when
+        val result = basic.sub(a, b)
+
+        //then
+        assertThat(result).isEqualTo(a - b)
+    }
+}
